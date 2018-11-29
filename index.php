d3e862da484c9d398401a52c2608fa2b48970fbf
Charis <tranhuudung1@gmail.com>
2018-11-29 13:57:03 +0700

    add bootstrap(fix)

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./GUI/bootstrap/css/bootstrap.css">
+
     <title>Document</title>
 </head>
 <body>
     <div id="container">
-        <div id="contact">
+        <div id="topbar">
             
         </div>
         <div id="header">
@@ -18,5 +20,7 @@
         <div id="content"></div>
         <div id="footer"></div>
     </div>
+    <script src="./GUI/bootstrap/js/jquery-3.3.1.min.js"></script>
+    <script src="./GUI/bootstrap/js/bootstrap.js"></script>
 </body>
 </html>

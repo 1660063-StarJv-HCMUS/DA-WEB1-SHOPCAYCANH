57b740f5854e08b3d6dd2f6ea01914e162f61288
Charis <tranhuudung1@gmail.com>
2018-12-22 17:02:42 +0700

    tạo DTO BUS DAO cho chi tiết đơn hàng,đơn hàng

---
+++ DTO/ChiTietDonHang_DTO.php
@@ -0,0 +1,17 @@
+<?php
+class DonHang
+{
+    var $MaChiTietDonDatHang;
+    var $SoLuong;
+    var $GiaBan;
+    var $MaDonDatHang;
+    var $MaSanPham;
+    public function __construct()
+    {
+        $this->MaChiTietDonDatHang = "";
+        $this->MaDonDatHang = "";
+        $this->GiaBan = 0;
+        $this->SoLuong = 0;
+        $this->MaSanPham = 0;
+    }
+}
---
+++ DTO/DonHang_DTO.php
@@ -0,0 +1,17 @@
+<?php
+class DonHang
+{
+    var $MaDonHang;
+    var $NgayLap;
+    var $TongThanhTien;
+    var $MaTaiKhoan;
+    var $MaTinhTrang;
+    public function __construct()
+    {
+        $this->MaDonHang = "";
+        $this->NgayLap = "";
+        $this->TongThanhTien = 0;
+        $this->MaDonHang = 0;
+        $this->MaTinhTrang = 0;
+    }
+}
